212cde8c56451cbfc4f3040933ed3010b61136b3
FcoMelendez <fco.melendez.f@gmail.com>
2024-07-23 15:17:14 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 html_theme_options = {
-    'sidebarbgcolor': '6a5183',
+    'sidebarbgcolor': 'purple',
 }
 
 # -- Options for EPUB output
